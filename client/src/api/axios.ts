2baa789a72373f08f9ec3b6f2513a3666f944b1e
bhardwajjshubh <shubhambhar99397@gmail.com>
2025-07-03 21:33:26 +0530

    Update vercel.json

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://otp-authentication-assignment.onrender.com/api', // deployed backend
+  baseURL: 'https://otp-authentication-assignment.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
